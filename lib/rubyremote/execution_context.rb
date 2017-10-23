80e51149ebf92a12f78be3f2b1e14fe7804f814e
Nikita Chernukhin <nuinuhin@gmail.com>
2017-10-23 11:51:29 +0200

    Sending locals from the client

--- lib/rubyremote/execution_context.rb
+++ lib/rubyremote/execution_context.rb
@@ -1,7 +1,7 @@
 require 'open3'
-# require 'parse_tree'
-# require 'parse_tree_extensions'
+require 'base64'
 require 'method_source'
+require 'colorize'
 
 module Rubyremote
   class ExecutionContext
@@ -16,8 +16,29 @@ module Rubyremote
       self.hash.abs
     end
 
-    def compile(ruby_code)
-      body = <<-RUBY
+    def compile(ruby_code, client_locals = {})
+      code = StringIO.new
+      code.puts("require('base64')")
+
+      if client_locals.any?
+        code.puts "CLIENT_LOCALS_NAMES = %i(#{client_locals.keys.join(' ')})"
+        code.puts "MARSHALLED_LOCALS_NAMES = CLIENT_LOCALS_NAMES + [:__return_val__#{id}]"
+      end
+
+      client_locals.each do |name, data|
+        bin_data = Marshal.dump(data)
+        base64_data = Base64.encode64(bin_data)
+        code.puts <<-RUBY
+          #{name} = begin
+            Marshal.load(Base64.decode64('#{base64_data}'))
+          rescue ArgumentError
+            STDERR.puts("Warning: could not resolve type for #{name} variable")
+            nil
+          end
+        RUBY
+      end
+
+      code.puts <<-RUBY
         __context_id__ = #{id}
 
         __return_val__#{id} = begin
@@ -27,12 +48,12 @@ module Rubyremote
         end
       RUBY
 
-      footer = <<-'RUBY'
+      code.puts <<-'RUBY'
         # Marshalling local variables and result
 
         puts "%%%MARSHAL_#{__context_id__}"
 
-        local_variables.each do |lv|
+        MARSHALLED_LOCALS_NAMES.each do |lv|
           data = Marshal.dump(eval(lv.to_s))
           data_length = data.size
           puts "#{lv}:#{data_length}"
@@ -40,11 +61,15 @@ module Rubyremote
         end
       RUBY
 
-      body + footer
+      code.string
     end
 
-    def execute(&bl)
-      execute_code(parse_block(bl.source))
+    def name
+      "#{server}:#{working_dir}"
+    end
+
+    def execute(locals = {}, &block)
+      execute_code(parse_block(block.source), **locals)
     end
 
     # Quick and dirty solution
@@ -53,15 +78,15 @@ module Rubyremote
       block_code.lines[1..-2].join
     end
 
-    def execute_code(ruby_code)
+    def execute_code(ruby_code, client_locals = {})
       res = nil
 
       remote_command = "\"cd #{working_dir} && ruby\""
       command = "ssh #{server} #{remote_command}"
 
       Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
-        code = compile(ruby_code)
-
+        code = compile(ruby_code, **client_locals)
+        # puts code
         stdin.write(code)
         stdin.close
 
@@ -79,6 +104,7 @@ module Rubyremote
               begin
                 locals[varname] = Marshal.load(data)
               rescue ArgumentError
+                STDERR.puts("Warning: could not resolve type for #{varname} variable")
                 locals[varname] = nil
               end
             end
@@ -87,11 +113,14 @@ module Rubyremote
 
             break
           else
-            puts "> #{line}"
+            puts "#{name.green}>\t#{line}"
           end
         end
 
-        STDERR.write(stderr.read)
+        until stderr.eof?
+          line = stderr.readline
+          STDERR.puts "#{name.red}>\t#{line}"
+        end
       end
 
       res
--- rubyremote.gemspec
+++ rubyremote.gemspec
@@ -31,6 +31,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_runtime_dependency "method_source"
+  spec.add_runtime_dependency "colorize"
 
   spec.add_development_dependency "bundler", "~> 1.15"
   spec.add_development_dependency "rake", "~> 10.0"
