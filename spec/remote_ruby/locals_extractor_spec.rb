5f21741677f160f20fda2e03e7be6445b77ce4d7
Nikita Chernukhin <nuinuhin@gmail.com>
2018-08-27 11:27:35 +0200

    Specs for LocalsExtractor

---
+++ spec/remote_ruby/locals_extractor_spec.rb
@@ -0,0 +1,30 @@
+describe ::RemoteRuby::LocalsExtractor do
+  def fetch_block(&block)
+    block
+  end
+
+  it 'extracts local variables' do
+    int_value = 5
+
+    block = fetch_block {}
+
+    extractor = described_class.new(block)
+    expect(extractor.locals).to include(int_value: int_value)
+  end
+
+  context 'with excluded types' do
+    it 'ignores specified types' do
+      int_value = 5
+      string_value = 'A string'
+
+      block = fetch_block do
+        puts int_value
+        puts string_value
+      end
+
+      extractor = described_class.new(block, ignore_types: String)
+      expect(extractor.locals).to include(:int_value)
+      expect(extractor.locals).not_to include(:string_value)
+    end
+  end
+end
