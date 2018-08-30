66d8324ce3e4959da4c7b9b457c4384e65ee0510
Nikita Chernukhin <nuinuhin@gmail.com>
2018-08-31 01:46:53 +0200

    Improve coverage

--- spec/remote_ruby/connection_adapter/local_stdin_adapter_spec.rb
+++ spec/remote_ruby/connection_adapter/local_stdin_adapter_spec.rb
@@ -12,26 +12,34 @@ describe ::RemoteRuby::LocalStdinAdapter do
     FileUtils.rm_rf(working_dir)
   end
 
-  it 'changes to the working dir' do
-    pwd = nil
-
-    adapter.open('puts Dir.pwd') do |stdout, _stderr|
-      pwd = stdout.read
-      pwd.strip!
+  describe '#connection_name' do
+    it 'equals to working_dir' do
+      expect(adapter.connection_name).to eq(working_dir)
     end
-
-    expect(pwd).to eq(working_dir)
   end
 
-  it 'is launched in a separate process' do
-    new_pid = nil
+  describe '#open' do
+    it 'changes to the working dir' do
+      pwd = nil
 
-    adapter.open('puts Process.pid') do |stdout, _stderr|
-      new_pid = stdout.read.to_i
+      adapter.open('puts Dir.pwd') do |stdout, _stderr|
+        pwd = stdout.read
+        pwd.strip!
+      end
+
+      expect(pwd).to eq(working_dir)
     end
 
-    expect(new_pid).not_to be_nil
-    expect(new_pid).not_to be_zero
-    expect(new_pid).not_to eq(Process.pid)
+    it 'is launched in a separate process' do
+      new_pid = nil
+
+      adapter.open('puts Process.pid') do |stdout, _stderr|
+        new_pid = stdout.read.to_i
+      end
+
+      expect(new_pid).not_to be_nil
+      expect(new_pid).not_to be_zero
+      expect(new_pid).not_to eq(Process.pid)
+    end
   end
 end
