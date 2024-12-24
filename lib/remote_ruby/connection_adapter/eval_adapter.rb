cf5956c7cb47108fd2bf500e5b0f290c4c491858
Nikita Chernukhin <nuinuhin@gmail.com>
2024-12-24 15:38:13 +0100

    Simplify Eval adapter

--- lib/remote_ruby/connection_adapter/eval_adapter.rb
+++ lib/remote_ruby/connection_adapter/eval_adapter.rb
@@ -4,47 +4,14 @@ module RemoteRuby
   # An adapter to expecute Ruby code in the current process in an isolated
   # scope
   class EvalAdapter < ConnectionAdapter
-    attr_reader :async, :working_dir
+    attr_reader :working_dir
 
-    def initialize(working_dir: Dir.pwd, async: false)
+    def initialize(working_dir: Dir.pwd)
       super
-      @async = async
       @working_dir = working_dir
     end
 
-    def open(code, &block)
-      if async
-        run_async(code, &block)
-      else
-        run_sync(code, &block)
-      end
-    end
-
-    private
-
-    def run_code(code)
-      binder = Object.new
-
-      Dir.chdir(working_dir) do
-        binder.instance_eval(code)
-      end
-    end
-
-    def run_sync(code)
-      with_stringio do |tmp_stdout, tmp_stderr|
-        with_tmp_streams($stdin, tmp_stdout, tmp_stderr) do
-          run_code(code)
-        end
-
-        tmp_stdout.close_write
-        tmp_stderr.close_write
-        tmp_stdout.rewind
-        tmp_stderr.rewind
-        yield nil, tmp_stdout, tmp_stderr
-      end
-    end
-
-    def run_async(code)
+    def open(code)
       with_pipes do |in_read, in_write, out_read, out_write, err_read, err_write|
         t = Thread.new do
           with_tmp_streams(in_read, out_write, err_write) do
@@ -61,6 +28,16 @@ module RemoteRuby
       end
     end
 
+    private
+
+    def run_code(code)
+      binder = Object.new
+
+      Dir.chdir(working_dir) do
+        binder.instance_eval(code)
+      end
+    end
+
     def with_pipes
       in_read, in_write = IO.pipe
       out_read, out_write = IO.pipe
@@ -70,16 +47,6 @@ module RemoteRuby
       in_read.close
       out_read.close
       err_read.close
-    end
-
-    def with_stringio
-      out = StringIO.new
-      err = StringIO.new
-
-      yield out, err
-    ensure
-      out.close
-      err.close
     end
 
     def with_tmp_streams(ins, out, err)
--- spec/remote_ruby/connection_adapter/eval_adapter_spec.rb
+++ spec/remote_ruby/connection_adapter/eval_adapter_spec.rb
@@ -1,85 +1,62 @@
 # frozen_string_literal: true
 
 describe RemoteRuby::EvalAdapter do
-  shared_context 'Process and IO behaviour' do
-    let(:working_dir) do
-      File.realpath(Dir.mktmpdir)
-    end
-
-    after(:each) do
-      FileUtils.rm_rf(working_dir)
-    end
-
-    it 'is launched in the same process' do
-      new_pid = nil
-
-      adapter.open('puts Process.pid') do |_stdin, stdout, _stderr|
-        new_pid = stdout.read.to_i
-      end
-
-      expect(new_pid).not_to be_nil
-      expect(new_pid).not_to be_zero
-      expect(new_pid).to eq(Process.pid)
-    end
-
-    it 'restores $stdout and $stderr variables' do
-      adapter.open('puts Process.pid') do |_stdin, stdout, stderr|
-        expect(stdout).not_to eq(STDOUT)
-        expect(stderr).not_to eq(STDERR)
-      end
-
-      expect($stdout).to eq(STDOUT)
-      expect($stderr).to eq(STDERR)
-    end
-
-    it 'changes to the working dir' do
-      pwd = nil
-      old_dir = Dir.pwd
-
-      adapter.open('puts Dir.pwd') do |_stdin, stdout, _stderr|
-        pwd = stdout.read
-        pwd.strip!
-      end
-
-      expect(pwd).to eq(working_dir)
-      expect(Dir.pwd).to eq(old_dir)
-    end
+  subject(:adapter) do
+    described_class.new(working_dir: working_dir)
   end
 
-  context 'synchronous' do
-    subject(:adapter) { described_class.new(working_dir: working_dir) }
-    include_context 'Process and IO behaviour'
-
-    it 'is launched in the same thread' do
-      new_thread_id = nil
-
-      adapter.open('puts Thread.current.object_id') do |_stdin, stdout, _stderr|
-        new_thread_id = stdout.read.to_i
-      end
-
-      expect(new_thread_id).not_to be_nil
-      expect(new_thread_id).not_to be_zero
-      expect(new_thread_id).to eq(Thread.current.object_id)
-    end
+  let(:working_dir) do
+    File.realpath(Dir.mktmpdir)
   end
 
-  context 'asynchronous' do
-    subject(:adapter) do
-      described_class.new(async: true, working_dir: working_dir)
+  after(:each) do
+    FileUtils.rm_rf(working_dir)
+  end
+
+  it 'is launched in the same process' do
+    new_pid = nil
+
+    adapter.open('puts Process.pid') do |_stdin, stdout, _stderr|
+      new_pid = stdout.read.to_i
     end
 
-    include_context 'Process and IO behaviour'
+    expect(new_pid).not_to be_nil
+    expect(new_pid).not_to be_zero
+    expect(new_pid).to eq(Process.pid)
+  end
 
-    it 'is launched in a different thread' do
-      new_thread_id = nil
-
-      adapter.open('puts Thread.current.object_id') do |_stdin, stdout, _stderr|
-        new_thread_id = stdout.read.to_i
-      end
-
-      expect(new_thread_id).not_to be_nil
-      expect(new_thread_id).not_to be_zero
-      expect(new_thread_id).not_to eq(Thread.current.object_id)
+  it 'restores $stdout and $stderr variables' do
+    adapter.open('puts Process.pid') do |_stdin, stdout, stderr|
+      expect(stdout).not_to eq(STDOUT)
+      expect(stderr).not_to eq(STDERR)
     end
+
+    expect($stdout).to eq(STDOUT)
+    expect($stderr).to eq(STDERR)
+  end
+
+  it 'changes to the working dir' do
+    pwd = nil
+    old_dir = Dir.pwd
+
+    adapter.open('puts Dir.pwd') do |_stdin, stdout, _stderr|
+      pwd = stdout.read
+      pwd.strip!
+    end
+
+    expect(pwd).to eq(working_dir)
+    expect(Dir.pwd).to eq(old_dir)
+  end
+
+  it 'is launched in a different thread' do
+    new_thread_id = nil
+
+    adapter.open('puts Thread.current.object_id') do |_stdin, stdout, _stderr|
+      new_thread_id = stdout.read.to_i
+    end
+
+    expect(new_thread_id).not_to be_nil
+    expect(new_thread_id).not_to be_zero
+    expect(new_thread_id).not_to eq(Thread.current.object_id)
   end
 end
